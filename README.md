db1d0f0f6d842db4da338408aa33ac69c1193236
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2024-02-13 05:58:42 +0900

    add testing sample
    add testing sample

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # test_mlit-dpf-api
 try MILT Data PlatForm API sample
+
+## MILT Data PlatForm
+国土交通データプラットフォーム
+https://www.mlit-data.jp/#/
+
+## sample
+国交 DPF 利用者 API、利用サンプルプログラム | 国土交通DPF利用者API
+https://www.mlit-data.jp/api_docs/examples/introduction.html
---
+++ test_simple_search_without_api_key.py
@@ -0,0 +1,71 @@
+import requests
+
+# エンドポイントとAPIキーを定義しておく
+END_POINT = "https://www.mlit-data.jp/api/v1/"
+API_KEY = "please use your own key"
+
+# 指定したキーワードと緯度経度の付近で最初にヒットした5件のデータ名（タイトル）の検索を行う。
+# keyword = 検索キーワード
+# lat = 十進数での緯度
+# lon = 十進数での経度
+# range = 度単位で検索する範囲を指定する。
+
+
+def get_data(keyword, lat, lon, range):
+    # GraphQLクエリー内容を作成
+    top = lat + range
+    bottom = lat - range
+    left = lon - range
+    right = lon + range
+    graph_ql_query = """
+    query {
+      search(
+        term: "%s",
+        first: 0,
+        size: 5,
+        locationFilter: {
+          rectangle: {
+            topLeft: {
+              lat: %f,
+              lon: %f
+            },
+            bottomRight: {
+              lat: %f,
+              lon: %f
+            }
+          }
+        }
+      ) {
+        totalNumber
+        searchResults {
+          title
+        }
+      }
+    }
+    """ % (keyword, top, left, bottom, right)
+
+    # APIを呼び出して結果を準備する。
+    result = []
+    response_data = requests.post(
+        END_POINT,
+        headers={
+            "Content-type": "application/json",
+            "apikey": API_KEY,
+        },
+        json={"query": graph_ql_query}).json()
+    print(response_data)
+
+    for data in response_data["data"]["search"]["searchResults"]:
+        result.append(data["title"])
+
+    return result
+
+
+# 検索の関数を呼び出して、結果を出力する。
+#sampleresult = get_data("橋梁", 35.69394069179055, 139.75364318486396, 0.01)
+#sampleresult = get_data("橋梁", 35.69394069179055, 139.75364318486396, 1.00)
+#sampleresult = get_data("橋梁", 35.69394069179055, 139.75364318486396, 1.00)
+#sampleresult = get_data("空港", 35.69394069179055, 139.75364318486396, 1.00)
+sampleresult = get_data("成田空港", 35.69394069179055, 139.75364318486396, 1.00)
+
+print(sampleresult)
